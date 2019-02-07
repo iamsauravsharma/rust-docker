28638174d456dc739a3208126d09845d851d40f2
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-02-07 11:33:54 +0545

    fix build docker script

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -22,7 +22,7 @@ do
         ./$os_name
 
         # build rustup for that os version
-        docker build --build-arg OS=$os_name --build-arg OS=$os_version \
+        docker build --build-arg OS=$os_name --build-arg OS_VERSION=$os_version \
         -t iamsauravsharma/rustup:$os_name-$os_version \
         ./rustup
 
@@ -31,22 +31,22 @@ do
         do
 
             # build only rust toolchain version installed docker
-            docker build --build-arg OS=$os_name --build-arg OS=$os_version --build-arg RUST=$rust_version \
+            docker build --build-arg OS=$os_name --build-arg OS_VERSION=$os_version --build-arg RUST_VERSION=$rust_version \
             -t iamsauravsharma/rust:$rust_version-$os_name$os_version \
             ./rust
 
             # build docker with clippy installed along with rust
-            docker build --build-arg OS=$os_name --build-arg OS=$os_version --build-arg RUST=$rust_version \
+            docker build --build-arg OS=$os_name --build-arg OS_VERSION=$os_version --build-arg RUST_VERSION=$rust_version \
             -t iamsauravsharma/rust-clippy:$rust_version-$os_name$os_version \
             ./rust-clippy
 
             # build docker with rustfmt installed along with rust
-            docker build --build-arg OS=$os_name --build-arg OS=$os_version --build-arg RUST=$rust_version \
+            docker build --build-arg OS=$os_name --build-arg OS_VERSION=$os_version --build-arg RUST_VERSION=$rust_version \
             -t iamsauravsharma/rust-fmt:$rust_version-$os_name$os_version \
             ./rust-fmt
 
             # build docker with both clippy and rustfmt installed along with rust
-            docker build --build-arg OS=$os_name --build-arg OS=$os_version --build-arg RUST=$rust_version \
+            docker build --build-arg OS=$os_name --build-arg OS_VERSION=$os_version --build-arg RUST_VERSION=$rust_version \
             -t iamsauravsharma/rust-fmt-clippy:$rust_version-$os_name$os_version \
             ./rust-fmt-clippy
         done
