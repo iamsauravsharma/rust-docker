cdcd6233959d38c7733858191f9bc2f42b97d5aa
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-07-11 13:13:55 +0545

    [skip ci] force remove a images as well as no prune
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- scripts/clean_docker.sh
+++ scripts/clean_docker.sh
@@ -4,4 +4,4 @@
 docker rm $(docker ps -aq) || echo "no container are present to remove out"
 
 #clean and remove images
-docker rmi $(docker images -q) || echo "no images are present to remove out"
+docker rmi --force --no-prune $(docker images -q) || echo "no images are present to remove out"
