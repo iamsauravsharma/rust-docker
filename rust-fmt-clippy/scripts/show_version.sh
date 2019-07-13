4ba7cc3656167d4005524319d439fd301e3835f5
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-07-13 15:54:41 +0545

    Remove script file and add all scripts in Dockerfile
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- rust-fmt-clippy/scripts/show_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-uname -a
-rustup --version
-rustc --version
-cargo --version
-cargo clippy --version
-rustfmt --version
-/bin/bash
--- ubuntu/scripts/add_package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt update
-apt dist-upgrade -y
-apt install -y curl sudo git ca-certificates --no-install-recommends
-apt autoremove -y
-apt clean -y
-rm -rf /var/lib/apt/lists/*
-rm -rf /tmp/* /var/tmp/*
